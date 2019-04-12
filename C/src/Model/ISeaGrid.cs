fa2369d2d62af2f233e3f21793d5b5391b8541fc
jan-vdm <janhvdm98@gmail.com>
2019-04-12 11:39:19 +1000

    Models

---
+++ C/src/Model/ISeaGrid.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Configuration;
+using System.Text;
+
+namespace MyGame.src.Model
+{
+    // '' <summary>
+    // '' The ISeaGrid defines the read only interface of a Grid. This
+    // '' allows each player to see and attack their opponents grid.
+    // '' </summary>
+    public interface ISeaGrid
+    {
+
+        int Width { get; }
+        int Height { get; }
+
+        EventHandler EventChanged();
+        TileView Item(int row, int column);
+        AttackResult HitTile(int row, int column);
+
+    }
+}
